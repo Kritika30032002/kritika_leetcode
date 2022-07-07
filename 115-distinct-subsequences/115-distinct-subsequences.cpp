997c1b747c3c57f94e4d0ef040a70d4143aeb1bc
Kritika30032002 <83400697+Kritika30032002@users.noreply.github.com>
2022-07-07 12:30:21 +0530

    Time: 96 ms (14.38%), Space: 18.2 MB (64.75%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+	//we use unsigned int because there is a case with a quite large data
+        vector<vector<unsigned int>> dp(t.size() + 1, vector<unsigned int>(s.size() + 1));
+        
+		//set as default value of  the first line as 1
+        for (int j = 0; j <= s.size(); j++)dp[0][j] = 1;
+
+//logic of the loop is above
+        for (int i = 1; i <= t.size(); i++) {
+            for (int j = 1; j <= s.size(); j++) {
+
+					if (t[i - 1] == s[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+                }
+                else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[dp.size() - 1][dp[0].size() - 1];
+    }
+};
+
+// For this problem explanation
+//https://leetcode.com/problems/distinct-subsequences/discuss/1472953/C%2B%2B-Solution-with-notes-and-image-Explanation
