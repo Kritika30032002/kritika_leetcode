a6b298762ca1319b622558a22d3a05a833850fe6
Kritika30032002 <83400697+Kritika30032002@users.noreply.github.com>
2022-07-09 11:05:23 +0530

    Time: 63 ms (52.33%), Space: 28.4 MB (41.80%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.cpp
@@ -10,3 +10,7 @@ public:
         return ans;
     }
 };
+
+//Take the minimum number from the array and then substarct that minimum number from all the elements in the array, the final answer is the sum of the difference between the minimum number and the integer values in the given array.
+//For example:
+//if the given array is [5,6,7,8] then the minimum value is 5. Substracting it from all the values given in the array will give us 5-5 + 6-5 + 7-5 + 8-5 =0 + 1 + 2 + 3 = 6.
