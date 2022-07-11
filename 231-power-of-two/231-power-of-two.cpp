4d4dec47ab5da135c0d55bd57f320a380f051292
Kritika30032002 <83400697+Kritika30032002@users.noreply.github.com>
2022-07-11 11:15:31 +0530

    Time: 3 ms (58.99%), Space: 5.9 MB (70.00%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -21,6 +21,9 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
         return ((n&(n-1))==0);
+        
+       // OR
+       //      return n > 0 ? !(n & n-1) : false;
     }
 };
 
