05d2e705f44bc0ae593d3e18424cc0831e0be0ed
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2022-07-24 12:44:55 +0530

    Time: 18 ms (28.34%), Space: 11.4 MB (16.56%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,48 +17,19 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node *iter = head; 
-          Node *front = head;
-
-          // First round: make copy of each node,
-          // and link them together side-by-side in a single list.
-          while (iter != NULL) {
-            front = iter->next;
-
-            Node *copy = new Node(iter->val);
-            iter->next = copy;
-            copy->next = front;
-
-            iter = front;
-          }
-
-          // Second round: assign random pointers for the copy nodes.
-          iter = head;
-          while (iter != NULL) {
-            if (iter->random != NULL) {
-              iter->next->random = iter->random->next;
-            }
-            iter = iter->next->next;
-          }
-
-          // Third round: restore the original list, and extract the copy list.
-          iter = head;
-          Node *pseudoHead = new Node(0);
-          Node *copy = pseudoHead;
-
-          while (iter != NULL) {
-            front = iter->next->next;
-
-            // extract the copy
-            copy->next = iter->next;
-
-            // restore the original list
-            iter->next = front;
-              
-            copy = copy -> next; 
-            iter = front;
-          }
-
-          return pseudoHead->next;
+        map<Node*, Node*> m;
+        int i=0;
+        Node* ptr = head;
+        while (ptr) {
+            m[ptr] =new Node(ptr->val);
+            ptr = ptr->next;
+        }
+        ptr = head;
+        while (ptr) {
+            m[ptr]->next = m[ptr->next];
+            m[ptr]->random = m[ptr->random];
+            ptr = ptr->next;
+        }
+        return m[head];
     }
 };
