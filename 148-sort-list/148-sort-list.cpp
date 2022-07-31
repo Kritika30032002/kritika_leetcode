e32f12a01527806bd5da41872718d12132928f7c
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2022-07-31 11:39:20 +0530

    Time: 419 ms (19.04%), Space: 53.4 MB (57.50%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -8,25 +8,79 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* sortList(ListNode* head) {
+//         ListNode* temp = head;
+//         vector<int> order;
+//         while(temp!=NULL)
+//         {
+//             order.push_back(temp->val);
+//             temp = temp->next;
+//         }
+        
+//         sort(order.begin() , order.end());
+//         temp = head;
+//         int index = 0;
+//         while(temp!=NULL)
+//         {
+//             temp->val = order[index++];
+//             temp = temp->next;
+//         }
+//         return head;
+//     }
+// };
+
+// or
+
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        ListNode* temp = head;
-        vector<int> order;
-        while(temp!=NULL)
-        {
-            order.push_back(temp->val);
-            temp = temp->next;
+    ListNode* midpoint(ListNode* head){
+        if(head == NULL or head->next == NULL){
+            return head;
         }
         
-        sort(order.begin() , order.end());
-        temp = head;
-        int index = 0;
-        while(temp!=NULL)
-        {
-            temp->val = order[index++];
-            temp = temp->next;
+        ListNode *slow = head, *fast = head->next;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return head;
+        
+        return slow;
+    }
+    
+    ListNode* mergeSortedLists(ListNode* a, ListNode* b){
+        if(a == NULL) return b;
+        if(b == NULL) return a;
+        
+        ListNode* temp;
+        if(a->val <= b->val){
+            temp = a;
+            temp->next = mergeSortedLists(a->next, b);
+        }
+        else{
+            temp = b;
+            temp->next = mergeSortedLists(a, b->next);
+        }
+        
+        return temp;
+    }
+    
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL or head->next == NULL){
+            return head;
+        }
+        
+        ListNode* mid = midpoint(head);
+        ListNode* a = head;
+        ListNode* b = mid->next;
+        
+        mid->next = NULL;
+        
+        a = sortList(a);
+        b = sortList(b);
+        
+        ListNode* temp = mergeSortedLists(a, b);
+        return temp;
     }
 };
