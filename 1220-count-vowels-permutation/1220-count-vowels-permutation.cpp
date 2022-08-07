d4fbb88af66e5bc8dd769ef52fdc5852d525d979
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2022-08-07 10:39:15 +0530

    Time: 3 ms (95.02%), Space: 6 MB (90.50%) - LeetHub

---
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -0,0 +1,27 @@
+// We use variables to save the number of strings ending with that letter.
+// We check the rules and see for each letter after which letter it can appear.
+// We use the second variables because we have to update in parallel.
+    
+class Solution {
+public:
+    int countVowelPermutation(int n) {
+        long a = 1, e = 1, i = 1, o = 1, u = 1, mod = pow(10, 9)+7;
+        long a2, e2, i2, o2, u2; 
+        
+        for (int j = 2; j <= n; j++) {
+            a2 = (e + i + u) % mod;
+            e2 = (a + i) % mod;
+            i2 = (e + o) % mod;
+            o2 = i;
+            u2 = (o + i) % mod;
+            
+            a = a2, e = e2, i = i2, o = o2, u = u2;
+        }
+        
+        return (a + e + i + o + u) % mod;
+    }
+};
+
+
+// Time Complexity : O(N)
+// Space Complexity : O(1)
