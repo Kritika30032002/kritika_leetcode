9111bbfb281d805f355f075d41afb1743d3ea546
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2022-10-02 10:17:23 +0530

    Time: 3 ms (63.67%), Space: 6 MB (47.41%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,6 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int count=0;
         while(n!=0){
+            
+            //checking last bit
             if(n&1){
                 count++;
             }
