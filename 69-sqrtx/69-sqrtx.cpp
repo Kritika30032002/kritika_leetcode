a2424f7dc13cd56da73079ba36c1aeb496ea641d
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2022-10-08 18:21:30 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (24.79%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
  int mySqrt(int x) {
-     if(x==0) return 0;
-int l=0, r=x;
-int res, mid;  
-     while(l<=r){             
-            mid=(l+r)/2;
-         if(mid == 0) return 1;
-            if(mid==x/mid) return mid;    
-            else if(mid<x/mid){             
-                l=mid+1;       
-                res = mid;      
+  long long s=0, e=x, ans, mid;   //long long due to some of test cases overflows integer limit.
+        while(s<=e){             
+            mid=(s+e)/2;
+            if(mid*mid==x) return mid;     //if the 'mid' value ever gives the result, we simply return it.
+            else if(mid*mid<x){             
+                s=mid+1;        //if 'mid' value encounterted gives lower result, we simply discard all the values lower than mid.
+                ans=mid;        //an extra pointer 'ans' is maintained to keep track of only lowest 'mid' value. 
             }
-            else r=mid-1;       
+            else e=mid-1;       //if 'mid' value encountered gives greater result, we simply discard all the values greater than mid. 
         }
-        return res;  
+        return ans;    
     }
 };
