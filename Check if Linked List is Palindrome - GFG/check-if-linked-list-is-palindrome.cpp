947741569697a9d80e3bf44007fb15de4426d87c
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2022-12-29 08:45:47 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -28,35 +28,108 @@ struct Node {
   }
 };
 */
+//--------------Using vector-----------------//
+//TIME COMPLEXITY: O(N)
+//SPACE COMPLEXITY:O(N)
 
-#include <vector>
-class Solution{
-    private:
-    bool checkPalindrome (vector <int> arr){
-        int n=arr.size();
-        int s=0,e=n-1;
+// #include <vector>
+// class Solution{
+//     private:
+//     bool checkPalindrome (vector <int> arr){
+//         int n=arr.size();
+//         int s=0,e=n-1;
         
-        while(s<=e){
-            if(arr[s]!=arr[e]){
-                return false;
-            }
-            s++; e--;
+//         while(s<=e){
+//             if(arr[s]!=arr[e]){
+//                 return false;
+//             }
+//             s++; e--;
+//         }
+//         return true;
+//     }
+//   public:
+//     //Function to check whether the list is palindrome.
+//     bool isPalindrome(Node *head)
+//     {
+//         //Your code here
+//         vector <int> arr;
+//         Node* temp=head;
+        
+//         while(temp!=NULL){
+//             arr.push_back(temp->data);
+//             temp=temp->next;
+//         }
+//         return checkPalindrome(arr);
+//     }
+// };
+
+
+//--------------Reversing the half list-----------------//
+//TIME COMPLEXITY: O(N)
+//SPACE COMPLEXITY:O(1)
+
+class Solution{
+  private:
+     Node* getMid(Node* head){
+         Node* slow=head;
+         Node* fast=head->next;
+         
+         while(fast!=NULL && fast->next!=NULL){
+             fast=fast->next->next;
+             slow=slow->next;
+         }
+         return slow;
+     }
+     
+     Node* reverse(Node* head){
+         Node* curr=head;
+         Node* prev=NULL;
+         Node* next=NULL;
+         
+         while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
         }
-        return true;
-    }
+        return prev;
+     }
   public:
     //Function to check whether the list is palindrome.
     bool isPalindrome(Node *head)
     {
-        //Your code here
-        vector <int> arr;
-        Node* temp=head;
         
-        while(temp!=NULL){
-            arr.push_back(temp->data);
-            temp=temp->next;
+         if(head -> next == NULL) {
+            return true;
         }
-        return checkPalindrome(arr);
+        
+        //step 1 -> find Middle
+        Node* middle = getMid(head);
+        
+        //step 2 -> reverse List after Middle
+        Node* temp=middle->next;
+        middle->next=reverse(temp);
+        
+        //step 3 -> Compare both halves
+        Node* head1=head;
+        Node* head2=middle->next;
+        
+        while(head2!=NULL){
+            if(head1->data!=head2->data){
+                return false;
+            }
+            head1=head1->next;
+            head2=head2->next;
+        }
+        
+        //step 4 - repeat step 2(optional) 
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        return true;
+        
+        
+       
     }
 };
 
