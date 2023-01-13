3d2208917482ca9efb5773b12fb4d870662243c1
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-01-13 10:23:22 +0530

    Added solution - LeetHub

--- Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.cpp
@@ -101,6 +101,47 @@ struct Node
 };
  */
 
+// TIME COMPLEXITY: O(N2)
+
+// class Solution{
+//     private:
+//     int height(struct Node* node){
+//         //base case
+//         if(node == NULL) {
+//             return 0;
+//         }
+        
+//         int left = height(node ->left);
+//         int right = height(node->right);
+        
+//         int ans = max(left, right) + 1;
+//         return ans;
+//     }
+//     public:
+//     //Function to check whether a binary tree is balanced or not.
+//     bool isBalanced(Node *root)
+//     {
+//         //  Your Code here
+//         if(root==NULL){
+//             return true;
+//         }
+        
+//         bool left=isBalanced(root->left);
+//         bool right=isBalanced(root->right);
+        
+//         bool diff=abs(height(root->left) - height(root->right))<=1;
+        
+//         if(left && right && diff){
+//             return 1;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+// };
+
+
+
 class Solution{
     private:
     int height(struct Node* node){
@@ -117,24 +158,37 @@ class Solution{
     }
     public:
     //Function to check whether a binary tree is balanced or not.
+    pair<bool,int> isBalancedFast(Node *root){
+        if(root==NULL){
+            pair<bool,int> p=make_pair(true,0);
+            return p;
+        }
+        
+        pair<int,int> left=isBalancedFast(root->left);
+        pair<int,int> right=isBalancedFast(root->right);
+        
+        bool leftAns=left.first;
+        bool rightAns=right.first;
+        
+        bool diff=abs(left.second - right.second)<=1;
+        
+        pair<bool,int> ans;
+        ans.second = max(left.second, right.second) + 1;
+        
+        if(leftAns && rightAns && diff){
+             ans.first=true;
+        }
+        else{
+           ans.first=false;
+        }
+        return ans;
+    }
+    
     bool isBalanced(Node *root)
     {
         //  Your Code here
-        if(root==NULL){
-            return true;
-        }
+         return isBalancedFast(root).first;
         
-        bool left=isBalanced(root->left);
-        bool right=isBalanced(root->right);
-        
-        bool diff=abs(height(root->left) - height(root->right))<=1;
-        
-        if(left && right && diff){
-            return 1;
-        }
-        else{
-            return false;
-        }
     }
 };
 
