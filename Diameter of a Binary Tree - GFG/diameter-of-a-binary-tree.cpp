fea83bbaac107c9d7bf67da6eb8bee21a30f7834
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-01-13 09:43:09 +0530

    Added solution - LeetHub

--- Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
+++ Diameter of a Binary Tree - GFG/diameter-of-a-binary-tree.cpp
@@ -91,38 +91,91 @@ struct Node
     }
 }; */
 
+//TIME COMPLEXITY: O(N2);
+
+// class Solution {
+//   private:
+//     int height(struct Node* node){
+//         // code here 
+//         if(node==NULL){
+//             return 0;
+//         }
+        
+//         int left=height(node->left);
+//         int right=height(node->right);
+        
+//         int ans=max(left,right)+1;
+        
+//         return ans;
+//     }
+//   public:
+//     // Function to return the diameter of a Binary Tree.
+//     int diameter(Node* root) {
+//         // Your code here
+//         if(root==NULL){
+//             return 0;
+//         }
+        
+//         int opt1=diameter(root->left);
+//         int opt2=diameter(root->right);
+//         int opt3=height(root->left)+height(root->right)+1;
+        
+//         int ans=max(opt1,max(opt2,opt3));
+        
+//         return ans;
+//     }
+// };
+
+
+//TIME COMPLEXITY: O(N);
+//SPACE COMPLEXITY: O(height of tree);
+
 class Solution {
   private:
     int height(struct Node* node){
-        // code here 
-        if(node==NULL){
+        //base case
+        if(node == NULL) {
             return 0;
         }
         
-        int left=height(node->left);
-        int right=height(node->right);
+        int left = height(node ->left);
+        int right = height(node->right);
         
-        int ans=max(left,right)+1;
-        
+        int ans = max(left, right) + 1;
         return ans;
     }
   public:
     // Function to return the diameter of a Binary Tree.
-    int diameter(Node* root) {
-        // Your code here
-        if(root==NULL){
-            return 0;
+    
+    pair<int,int> diameterFast(Node* root) {
+        //base case
+        if(root == NULL) {
+            pair<int,int> p = make_pair(0,0);
+            return p;
         }
         
-        int opt1=diameter(root->left);
-        int opt2=diameter(root->right);
-        int opt3=height(root->left)+height(root->right)+1;
+        pair<int,int> left = diameterFast(root->left);
+        pair<int,int> right = diameterFast(root->right);
         
-        int ans=max(opt1,max(opt2,opt3));
+        int op1 = left.first;
+        int op2 = right.first;
+        int op3 = left.second + right.second + 1;
         
+        pair<int,int> ans;
+        ans.first = max(op1, max(op2, op3));;
+        ans.second = max(left.second , right.second) + 1;
+
         return ans;
     }
+    int diameter(Node* root) {
+    
+        return diameterFast(root).first;
+        
+    }
 };
+
+
+
 
 //{ Driver Code Starts.
 
