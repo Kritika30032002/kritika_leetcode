516e3d1ce1c1955e1b8cfe8b55f06e7670c66e87
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-05-08 18:24:53 +0530

    Time: 11 ms (87.18%), Space: 11.2 MB (64.67%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        int sum = 0;
-        for(int i=0;i<mat.size();i++){
-            for(int j=0;j<mat.size();j++){
-                if(i==j || i+j==mat.size()-1){
-                    sum+=mat[i][j];
-                }
-            }
-        }
-        return sum;
+        
+        // side length
+        int n = mat.size();
+        
+        // mid point index
+        int mid = n / 2;
+        
+        // summation of diagonal element
+        int summation = 0;
+        
+        for( int i = 0 ; i < n ; i++ ){      
+            // primary diagonal
+            summation += mat[i][i];
+            
+            // secondary diagonal
+            summation += mat[n-1-i][i];
+        }       
+        if( n % 2 == 1 ){           
+            // remove center element (repeated) on odd side-length case
+            summation -= mat[mid][mid];
+        }      
+        return summation;
     }
 };
