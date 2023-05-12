d39c76b04d218007ba7edb61e0278899804b70ed
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-05-12 17:47:04 +0530

    Time: 387 ms (42.70%), Space: 123.2 MB (19.10%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    long long solve(vector<vector<int>>& q,int i,vector<long long> &dp){
+        if(i>=q.size())
+             return 0;
+        
+        if(dp[i]!=-1)
+            return dp[i];
+        
+        long long taken=q[i][0]+ solve(q,i+q[i][1]+1,dp);
+        long long not_taken=solve(q,i+1,dp);
+        
+        return dp[i]=max(taken,not_taken);
+    }
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n=questions.size();
+        vector<long long> dp(n+1,-1);
+        return solve(questions,0,dp);
+    }
+};
