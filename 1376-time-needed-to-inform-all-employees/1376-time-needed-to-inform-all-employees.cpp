e280cfd73fececd296c8e2cb3e3a3ceb065d4482
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-06-03 17:16:40 +0530

    Time: 718 ms (8.51%), Space: 165.9 MB (5.82%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,19 +1,35 @@
+
+//BFS --- O(N)
 class Solution {
 public:
-    int max_time=INT_MIN;
+     int max_time=INT_MIN;
     
-    void DFS(map<int,vector<int>> &adj, vector<int> &informTime ,int curr_employee, int curr_time){
+void BFS(map<int,vector<int>> &adj, vector<int> &informTime ,int headID){
         
-        max_time=max(curr_time,max_time);
-        
+     queue<pair<int,int>> que;
+         
+     que.push({headID,0});
+         
+     while(!que.empty()){
+         
+         pair<int,int> temp=que.front();
+         
+         int curr_employee = temp.first;
+         int curr_time = temp.second;
+         
+         max_time=max(curr_time,max_time);
+         
+         que.pop();
+         
+         
         for(auto &subordinate: adj[curr_employee]){
-         DFS(adj, informTime, subordinate, curr_time+informTime[curr_employee] );
+            que.push({subordinate, curr_time+informTime[curr_employee]});
         }
+     }    
     }
     
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        
-        map<int,vector<int>> adj;
+          map<int,vector<int>> adj;
         
         for(int i=0;i<manager.size();i++){
             int child=i;
@@ -23,7 +39,45 @@ public:
                  adj[manager[i]].push_back(child);
         }
         
-        DFS(adj,informTime,headID,0);
+        BFS(adj,informTime,headID);
         return max_time;
     }
 };
+
+
+
+
+
+
+
+
+// DFS----- O(N)
+// class Solution {
+// public:
+//     int max_time=INT_MIN;
+    
+//     void DFS(map<int,vector<int>> &adj, vector<int> &informTime ,int curr_employee, int curr_time){
+        
+//         max_time=max(curr_time,max_time);
+        
+//         for(auto &subordinate: adj[curr_employee]){
+//          DFS(adj, informTime, subordinate, curr_time+informTime[curr_employee] );
+//         }
+//     }
+    
+//     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        
+//         map<int,vector<int>> adj;
+        
+//         for(int i=0;i<manager.size();i++){
+//             int child=i;
+//             int currmanager=manager[i];
+            
+//             if(manager[i]!=-1)
+//                  adj[manager[i]].push_back(child);
+//         }
+        
+//         DFS(adj,informTime,headID,0);
+//         return max_time;
+//     }
+// };
