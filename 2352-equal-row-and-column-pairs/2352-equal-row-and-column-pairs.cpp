9cecaa4bb872f2b20fc75b5ed691eea60c683ee0
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-06-13 23:21:02 +0530

    Time: 101 ms (81.17%), Space: 32.6 MB (61.06%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,3 +1,31 @@
+//BRUTE FORCE SOLUTION O(n3)
+
+// class Solution {
+// public:
+//     int equalPairs(vector<vector<int>>& grid) {
+//         int n=grid.size();
+        
+//         int count=0;
+        
+//         for(int r=0;r<n;r++){    
+//             for(int c=0;c<n;c++){
+//                 bool isEqual=true;
+//                 for(int i=0;i<n;i++){
+//                     if(grid[r][i]!=grid[i][c]){
+//                         isEqual=false;
+//                         break;
+//                     }                  
+//                 }
+//                 if(isEqual)
+//                     count++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+
+//OPTIMIZED SOLUTION - O(N2)
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
@@ -5,19 +33,21 @@ public:
         
         int count=0;
         
+        map<vector<int>,int> mp;
+        
         for(int r=0;r<n;r++){    
-            for(int c=0;c<n;c++){
-                bool isEqual=true;
-                for(int i=0;i<n;i++){
-                    if(grid[r][i]!=grid[i][c]){
-                        isEqual=false;
-                        break;
-                    }                  
-                }
-                if(isEqual)
-                    count++;
-            }
+              mp[grid[r]]++;                
         }
+        
+        for(int c=0;c<n;c++){
+            vector<int> temp;
+           for(int r=0;r<n;r++){    
+              temp.push_back(grid[r][c]);                
+           }
+            count+=mp[temp];
+        }
+        
         return count;
     }
 };
+
