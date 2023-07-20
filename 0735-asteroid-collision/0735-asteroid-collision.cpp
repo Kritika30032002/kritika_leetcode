a988ab3c58c50c4e230cf4c82c826f2416bd1c6d
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-07-20 22:17:53 +0530

    Time: 10 ms (93.53%), Space: 17.5 MB (81.53%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int>  ans;
+        
+        for(int &a : asteroids){
+            
+            while(!ans.empty() && a<0 && ans.back()>0){
+                int sum=a+ans.back();
+                
+                if(sum<0){
+                    ans.pop_back();
+                }
+                else if(sum>0){
+                    a=0;
+                    break;
+                }
+                else{
+                    ans.pop_back();
+                    a=0;
+                }
+               
+            }
+             
+         if(a!=0)
+            ans.push_back(a);
+        }
+        return ans;
+    }
+};
