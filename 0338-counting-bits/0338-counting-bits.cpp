f6b1db16c008850f7d967d99f51ca717843d07de
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-09-01 09:27:25 +0530

    Time: 3 ms (89.82%), Space: 7.9 MB (65.80%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -9,7 +9,10 @@ public:
         ans[0]=0;
         
         for(int i=1;i<=n;i++){
-            ans[i]=__builtin_popcount(i);
+            if(i%2 != 0)
+                ans[i]=ans[i/2]+1;
+            else
+                ans[i]=ans[i/2];
         }
         return ans;
     }
