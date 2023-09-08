e43648b5dcfd6c39bf7016fdda225df4d0c43291
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-09-08 15:41:40 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (82.50%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans(numRows);
+    vector<vector<int>>ans(numRows);
 
   for(int i=0;i<numRows;i++){
     ans[i].resize(i+1);
