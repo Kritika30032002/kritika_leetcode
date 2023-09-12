689df44b87cee7e7adbe5ea8956d310aec87247a
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-09-12 16:08:19 +0530

    Time: 143 ms (47.74%), Space: 121.1 MB (94.84%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -9,3 +9,11 @@ public:
         return ans + (nums.size() - ans) % 2;
     }
 };
+
+// every time we find a duplicate we will increment ans by 1.
+
+// thus to check the second condition we will subtract the ans value from the index as we are shifting the element to the left.
+
+// for the last edge case we will check the remaining size after removing ans element from the vector if the result is odd we have to remove 1 more element else just return the ans.
+    
+    
