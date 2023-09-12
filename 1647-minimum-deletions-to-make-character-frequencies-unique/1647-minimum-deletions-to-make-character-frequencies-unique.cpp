ede001adc9364b2ad3afc8ad4d8c8f6b2979db53
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-09-12 16:21:32 +0530

    Time: 46 ms (87.36%), Space: 17.2 MB (77.52%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -7,18 +7,45 @@ public:
             freq[ch-'a']++;
         }
         
-        int result=0;
-        unordered_set<int> st;
+        sort(begin(freq),end(freq));
         
-        for(int i=0;i<26;i++){
-            
-            while(freq[i] > 0 && st.find(freq[i]) != st.end()){
-                freq[i]-=1;
-                result++;
+        int result=0;
+        
+        for(int i=24;i>=0 && freq[i]>0;i--){
+            if(freq[i] >= freq[i+1]){
+                int prev=freq[i];
+                freq[i]=max(0,freq[i+1]-1);
+                result+=(prev-freq[i]);
             }
-            
-            st.insert(freq[i]);
         }
+        
+        
         return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     int minDeletions(string s) {
+//         int freq[26] = {0};
+        
+//         for(char &ch : s){
+//             freq[ch-'a']++;
+//         }
+        
+//         int result=0;
+//         unordered_set<int> st;
+        
+//         for(int i=0;i<26;i++){
+            
+//             while(freq[i] > 0 && st.find(freq[i]) != st.end()){
+//                 freq[i]-=1;
+//                 result++;
+//             }
+            
+//             st.insert(freq[i]);
+//         }
+//         return result;
+//     }
+// };
