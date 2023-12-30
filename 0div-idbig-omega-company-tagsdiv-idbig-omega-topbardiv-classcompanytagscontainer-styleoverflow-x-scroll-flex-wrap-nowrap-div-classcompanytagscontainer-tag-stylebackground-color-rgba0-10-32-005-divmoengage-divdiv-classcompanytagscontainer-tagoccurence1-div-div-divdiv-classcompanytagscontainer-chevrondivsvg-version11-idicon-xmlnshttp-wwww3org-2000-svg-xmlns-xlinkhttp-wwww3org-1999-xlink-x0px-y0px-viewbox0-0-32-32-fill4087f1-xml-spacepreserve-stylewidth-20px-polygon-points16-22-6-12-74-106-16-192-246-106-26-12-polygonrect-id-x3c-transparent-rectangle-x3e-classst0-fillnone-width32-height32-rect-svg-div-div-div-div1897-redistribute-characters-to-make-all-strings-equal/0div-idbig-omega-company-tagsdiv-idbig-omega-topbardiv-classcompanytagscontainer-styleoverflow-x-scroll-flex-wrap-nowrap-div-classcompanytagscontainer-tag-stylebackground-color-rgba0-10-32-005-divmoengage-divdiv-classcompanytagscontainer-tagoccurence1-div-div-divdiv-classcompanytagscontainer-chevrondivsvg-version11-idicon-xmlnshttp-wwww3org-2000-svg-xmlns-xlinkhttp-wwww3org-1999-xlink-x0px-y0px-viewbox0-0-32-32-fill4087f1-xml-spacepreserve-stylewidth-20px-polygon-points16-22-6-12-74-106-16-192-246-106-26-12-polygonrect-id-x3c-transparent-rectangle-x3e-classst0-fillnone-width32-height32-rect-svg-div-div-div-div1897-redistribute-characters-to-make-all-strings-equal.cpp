336a6e9f4175a8ec69867accbe7c47ed25101fd1
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2023-12-30 15:05:37 +0530

    Time: 19 ms (24.90%), Space: 11.9 MB (92.22%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmoengage-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1897-redistribute-characters-to-make-all-strings-equal/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmoengage-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+          int n = words.size();
+        int count[26] = {0};
+        
+        for (string &word : words) {
+            for (char &ch : word) {
+                ++count[ch - 'a'];
+            }
+        }
+        
+        auto lambda = [&](int c) {
+                   return c % n == 0;
+        };
+        
+        return all_of(begin(count), end(count), lambda);
+    }
+};
