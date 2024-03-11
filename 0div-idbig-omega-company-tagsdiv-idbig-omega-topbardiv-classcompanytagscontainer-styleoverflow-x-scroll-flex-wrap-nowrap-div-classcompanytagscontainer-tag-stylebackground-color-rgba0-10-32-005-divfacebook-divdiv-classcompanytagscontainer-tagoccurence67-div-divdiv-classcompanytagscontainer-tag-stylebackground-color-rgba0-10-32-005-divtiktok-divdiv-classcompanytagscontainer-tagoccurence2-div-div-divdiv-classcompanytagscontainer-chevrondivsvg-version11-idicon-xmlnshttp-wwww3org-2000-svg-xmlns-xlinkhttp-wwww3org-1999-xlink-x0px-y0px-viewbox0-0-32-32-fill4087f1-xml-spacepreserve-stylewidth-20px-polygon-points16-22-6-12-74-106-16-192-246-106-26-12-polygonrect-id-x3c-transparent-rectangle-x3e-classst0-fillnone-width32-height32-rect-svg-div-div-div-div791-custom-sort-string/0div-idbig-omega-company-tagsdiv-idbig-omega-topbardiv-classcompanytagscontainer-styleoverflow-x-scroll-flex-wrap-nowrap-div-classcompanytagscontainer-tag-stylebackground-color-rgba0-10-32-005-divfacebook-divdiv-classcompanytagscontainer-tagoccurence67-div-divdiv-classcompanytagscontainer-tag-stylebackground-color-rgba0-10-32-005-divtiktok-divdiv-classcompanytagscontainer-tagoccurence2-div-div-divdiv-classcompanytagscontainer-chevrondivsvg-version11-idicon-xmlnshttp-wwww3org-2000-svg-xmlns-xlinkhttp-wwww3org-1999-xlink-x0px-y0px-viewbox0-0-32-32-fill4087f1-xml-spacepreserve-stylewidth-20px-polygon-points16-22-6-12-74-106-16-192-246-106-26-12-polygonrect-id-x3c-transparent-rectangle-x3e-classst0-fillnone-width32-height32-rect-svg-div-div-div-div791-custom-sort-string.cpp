c79087ae45f9cd92a07b4bf5712552f8cc356e56
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2024-03-11 09:56:10 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (30.10%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence67-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div791-custom-sort-string/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence67-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divtiktok-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div791-custom-sort-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string customSortString(string order, string str) {
+         vector<int> index(26, INT_MAX);
+        
+        for(int i = 0; i<order.length(); i++)
+            index[order[i]-'a'] = i;
+        
+        
+        auto lambda = [&](char &ch1, char &ch2) {
+            if(index[ch1-'a'] == index[ch2-'a'])
+                return ch1 < ch2;
+            
+            return index[ch1-'a'] < index[ch2-'a'];
+        };
+        
+        sort(begin(str), end(str), lambda);
+        return str;
+        
+    }
+};
